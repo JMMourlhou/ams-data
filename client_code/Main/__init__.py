89852924dfb4241c4af3cfe88394c58c731384ac
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-09 16:32:39 +0000

    Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -7,11 +7,11 @@ import anvil.users
 
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import signup_for_AMS_Data
-from signup_for_AMS_Data.Form1 import Form1
+#import signup_for_AMS_Data
+#from signup_for_AMS_Data.Form1 import Form1
 
 class Main(MainTemplate):
-    def __init__(self, **properties):
+    def __init__(self, nb=2, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
@@ -22,8 +22,8 @@ class Main(MainTemplate):
         """This method is called when the button is clicked"""
         """Will call the EXTERNAL MODULE DEPENDACY when the link is clicked"""
                 
-        #import fr_custom_signup
-        #from fr_custom_signup.Form1 import Form1
+        import sign_up_for_AMS_Data
+        from sign_up_for_AMS_Data.Form1 import Form1
                
         self.content_panel.clear()
         self.content_panel.add_component(Form1(), full_width_row=True)
