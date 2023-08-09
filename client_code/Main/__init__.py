27768ccc842e88b40d2e97d9b58534d5c70b7a26
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-09 15:12:34 +0000

    Created Main and Fond bleu1.jpg; Edited theme.css and settings; Deleted Form1

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import MainTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
 
-class Form1(Form1Template):
+class Main(MainTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
