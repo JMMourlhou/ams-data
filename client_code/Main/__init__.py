eafc5ce2fec81c2d8bbbe017edc5d69753dfff32
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-10 11:58:06 +0000

    Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -23,9 +23,10 @@ class Main(MainTemplate):
         """This method is called when the button is clicked"""
         """Will call the EXTERNAL MODULE DEPENDACY when the link is clicked"""
                 
-        import sign_up_for_AMS_Data
-        from sign_up_for_AMS_Data.Form1 import Form1
-               
+        #import sign_up_for_AMS_Data
+        #from sign_up_for_AMS_Data.Form1 import Form1
+        import fr_custom_signup
+        from        
         self.content_panel.clear()
         self.content_panel.add_component(Form1(), full_width_row=True)
 
