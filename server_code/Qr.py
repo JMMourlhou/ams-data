a4033cd551273c49ba39786ba63dfcc67710bd55
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-13 19:39:58 +0000

    Edited custom package dependencies and Qr

--- server_code/Qr.py
+++ server_code/Qr.py
@@ -12,17 +12,21 @@ import anvil.server
 #import qrcode
 import io
 import qrcode
-import png
+#import png
+import PIL.Image as Image
+import base64
 
 from . import French_zone # importation du module pour le calcul du jour / heure
 
 @anvil.server.callable   
 def qr_generator(data="https://sxgqveyu3c2nj5kr.anvil.app/BBDLB54NWHHYKEE3YYGJGU4G"):
-    img = qrcode.make(data)
-    my_img= png.img.save("small_smiley.png")
+    img1 = qrcode.make(data)
+    b=base64.b64decode(img1)
+    print(b)
+    img = Image.open(io.BytesIO(b))
       
         
     # saving the image in my table 'Qr_codes'
     app_tables.qr_codes.add_row(time= French_zone.french_zone_time(),
                           description="essai",
-                          qr_code=my_img)
+                          qr_code=img)
