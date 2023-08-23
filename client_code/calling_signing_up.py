c8f85a275eb7a89ac571649b556aed9e9e89cc9d
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-23 13:12:43 +0000

    Edited calling_signing_up

--- client_code/calling_signing_up.py
+++ client_code/calling_signing_up.py
@@ -11,7 +11,7 @@ mais seulement d'un module !
 #import sign_up_for_AMS_Data
 from sign_up_for_AMS_Data.Form1 import Form1
 
-def calling_form1(h={}, num_stage):
+def calling_form1(h={}, num_stage=0):
    open_form('sign_up_for_AMS_Data.Form1',h, num_stage)
 
 
