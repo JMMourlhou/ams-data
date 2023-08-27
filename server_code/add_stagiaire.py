2eb43919d3768a11b97876aca75c4176c5b55fc6
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-27 20:56:19 +0000

    Edited add_stagiaire

--- server_code/add_stagiaire.py
+++ server_code/add_stagiaire.py
@@ -12,8 +12,8 @@ def add_stagiaire(stagiaire, stage, mode_fi):
 
     # lecture fichier père stages
     code_stage = app_tables.stages.get(numero=int(stage))
-    if len(code_stage)==0 :
-        alert("Stage non trouvé ds fichier stages !")
+    if not code_stage :
+        print("Stage non trouvé ds fichier stages !")
         valid=False
         return valid
 
@@ -21,14 +21,14 @@ def add_stagiaire(stagiaire, stage, mode_fi):
     user=anvil.users.get_user()
     if user:
         if user != stagiaire :
-            alert("Stagiaire non trouvé ds fichier users !")
+            print("Stagiaire non trouvé ds fichier users !")
             valid=False
             return valid
 
     # lecture fichier père mode financemnt
     mode_fin = app_tables.mode_financement.get(code_fi=mode_fi)    
-    if len(mode_fi)==0 :
-        alert("Mode de financemnt non trouvé ds fichier param mode financemnt !")
+    if not mode_fin :
+        print("Mode de financemnt non trouvé ds fichier param mode financemnt !")
         valid=False
         return valid   
         
@@ -39,11 +39,18 @@ def add_stagiaire(stagiaire, stage, mode_fi):
                               )
              
     stagiaire_row = app_tables.stagiaires_inscrits.search(stage=new_row['stage'])
-    if len(stagiaire_row)>0:
+    if stagiaire_row:
         valid=True
-        #effacemnt du code satge ds user:
+        #effacemnt du code stage ds user:
         user.update(stage_num_temp=None)
         
+        #incrément du nb de stagiaires en début de stage
+        """
+        nb_stagiaire_au_dbt=code_stage["nb_stagiaires_deb"]
+        nb=int(nb_stagiaire_au_dbt)
+        nb= nb + 1
+        code_stage.update(nb_stagiaires_deb=nb)
+        """
     else:
         valid=False
     return valid
