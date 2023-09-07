10aed361031a6a3f84d27de8e95047519694de2a
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-09-07 19:30:31 +0000

    Edited Visu_trombi; Deleted Form1

--- client_code/Visu_trombi/__init__.py
+++ client_code/Visu_trombi/__init__.py
@@ -52,95 +52,106 @@ class Visu_trombi(Visu_trombiTemplate):
                 if (cpt_ligne // 4) * 4 == cpt_ligne:                 # chgt de ligne
                     cpt_ligne += 1
 
-                esp = 150 # espace en pixel
-                
+                larg = 150 # largeur image en pixel
+                inter = 5  # Interval entre image
+                size_bt = 100 # largeur du bt transparent pour permettre le click
+                xx = 1
+                                
                 if cpt_ligne == 1:
                     if cpt_stagiaire == 1:
                         im1 = Image(background="white", 
                                     display_mode="shrink_to_fit",
-                                    height = esp,
+                                    height = larg,
                                     source = thumb_pic,
                                     horizontal_align = "center",
                                    )
-                        self.xy_panel.add_component(im1, x=1, y=1, width= esp)
+                        self.xy_panel.add_component(im1, x=1, y=1, width = larg)
                         # création du lien pour cliquer dessus
-                        bt_1 = Button(text="    ", background="", foreground="black", font_size=100)
+                        bt_1 = Button(text="    ", background="", foreground="black", font_size = size_bt)
                         self.xy_panel.add_component(bt_1, x=1, y=1)
                         bt_1.set_event_handler('click',self.bt_1_click)
+
+                    xx = xx + inter + larg
                         
                     if cpt_stagiaire == 2:
                         im2 = Image(background="white",
                                     display_mode="shrink_to_fit",
-                                    
-                                    height = esp,
+                                    height = larg,
                                     source = thumb_pic,
                                     horizontal_align = "center",
                                     visible = True
                                 )
-                        self.xy_panel.add_component(im2, x=esp*1, y=1, width = esp)
+                        self.xy_panel.add_component(im2, x=xx, y=1, width = larg)
                         # création du lien pour cliquer dessus
-                        bt_2 = Button(text="    ", background="", foreground="black", font_size=100)
-                        self.xy_panel.add_component(bt_2, x=esp*1, y=1)
+                        bt_2 = Button(text="    ", background="", foreground="black", font_size=size_bt)
+                        self.xy_panel.add_component(bt_2, x=xx, y=1)
                         bt_2.set_event_handler('click',self.bt_2_click)
                         
+                    xx = xx + inter + larg  
+                    
                     if cpt_stagiaire == 3:
                         im3 = Image(background="white",
                                     display_mode="shrink_to_fit",
-                                    
-                                    height = esp,
+                                    height = larg,
                                     source = thumb_pic,
                                     horizontal_align = "center",
                                     visible = True
                                 )
-                        self.xy_panel.add_component(im3, x=esp*2, y=1, width = esp)
+                        self.xy_panel.add_component(im3, x=xx, y=1, width = larg)
                         # création du lien pour cliquer dessus
-                        bt_3 = Button(text="    ", background="", foreground="black", font_size=100)
-                        self.xy_panel.add_component(bt_3, x=esp*2, y=1)
+                        bt_3 = Button(text="    ", background="", foreground="black", font_size=size_bt)
+                        self.xy_panel.add_component(bt_3, x=xx, y=1)
                         bt_3.set_event_handler('click',self.bt_3_click)
+
+                    xx = xx + inter + larg
 
                     if cpt_stagiaire == 4:
                         im4 = Image(background="white",
                                     display_mode="shrink_to_fit",
                                     
-                                    height = esp,
+                                    height = larg,
                                     source = thumb_pic,
                                     horizontal_align = "center",
                                     visible = True
                                 )
-                        self.xy_panel.add_component(im4, x=esp*3, y=1, width = esp)
+                        self.xy_panel.add_component(im4, x=xx, y=1, width = larg)
                         # création du lien pour cliquer dessus
                         bt_4 = Button(text="    ", background="", foreground="black", font_size=100)
-                        self.xy_panel.add_component(bt_4, x=esp*3, y=1)
+                        self.xy_panel.add_component(bt_4, x=xx, y=1)
                         bt_4.set_event_handler('click',self.bt_4_click)
+
+                    xx = xx + inter + larg
 
                     if cpt_stagiaire == 5:
                         im5 = Image(background="white",
                                     display_mode="shrink_to_fit",
                                     
-                                    height = esp,
+                                    height = larg,
                                     source = thumb_pic,
                                     horizontal_align = "center",
                                     visible = True
                                 )
-                        self.xy_panel.add_component(im5, x=esp*4, y=1, width = esp)
+                        self.xy_panel.add_component(im5, x=xx, y=1, width = larg)
                         # création du lien pour cliquer dessus
                         bt_5 = Button(text="    ", background="", foreground="black", font_size=100)
-                        self.xy_panel.add_component(bt_5, x=esp*4, y=1)
+                        self.xy_panel.add_component(bt_5, x=xx, y=1)
                         bt_5.set_event_handler('click',self.bt_5_click)
+
+                    xx = xx + inter + larg
 
                     if cpt_stagiaire == 6:
                         im6 = Image(background="white",
                                     display_mode="shrink_to_fit",
                                     
-                                    height = esp,
+                                    height = larg,
                                     source = thumb_pic,
                                     horizontal_align = "center",
                                     visible = True
                                 )
-                        self.xy_panel.add_component(im6, x=esp*5, y=1, width = esp)
+                        self.xy_panel.add_component(im6, x=xx, y=1, width = larg)
                         # création du lien pour cliquer dessus
                         bt_6 = Button(text="    ", background="", foreground="black", font_size=100)
-                        self.xy_panel.add_component(bt_6, x=esp*5, y=1)
+                        self.xy_panel.add_component(bt_6, x=xx, y=1)
                         bt_6.set_event_handler('click',self.bt_6_click)
                 else:
                     " pas premiere ligne "
