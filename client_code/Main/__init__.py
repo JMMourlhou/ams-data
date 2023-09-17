fd03888f622bbb85cc5a93f196437b6450e53c08
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-09-17 15:25:07 +0000

    Edited Main and settings; Deleted Start

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -4,7 +4,7 @@ import anvil.server
 #import anvil.tables as tables
 #import anvil.tables.query as q
 #from anvil.tables import app_tables
-from .. import Start
+
 from .. import French_zone
 from .. import calling_signing_up
 #import anvil.google.auth, anvil.google.drive
