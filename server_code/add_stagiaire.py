06053509fe0d63cd5d1b88aaf3971300b5091262
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-09-29 17:26:15 +0000

    Edited add_stagiaire

--- server_code/add_stagiaire.py
+++ server_code/add_stagiaire.py
@@ -18,11 +18,14 @@ def add_stagiaire(stagiaire, stage, mode_fi):
         valid=False
         return valid
     else:
-        # Incrément nb se stagiaires début stage 
+        # Incrément nb de stagiaires début stage 
         nb = int(code_stage['nb_stagiaires_deb'])
+        
+        print("Incrément avant ",nb)
         nb =+ 1
         code_stage.update(nb_stagiaires_deb=nb)
-
+        print("Incrément après ",nb)
+    
     # lecture fichier père user
     user=anvil.users.get_user()
     if user:
@@ -35,6 +38,13 @@ def add_stagiaire(stagiaire, stage, mode_fi):
     mode_fin = app_tables.mode_financement.get(code_fi=mode_fi)    
     if not mode_fin :
         print("Mode de financemnt non trouvé ds fichier param mode financemnt !")
+        valid=False
+        return valid   
+
+    #vérification si user pas déjà inscrit ds ce stage:
+    test = app_tables.stagiaires_inscrits.get(user_email=user)    
+    if test :
+        print("Vous ^tes déjà inscrit à ce stage !")
         valid=False
         return valid   
         
