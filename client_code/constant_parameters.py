5864119561d91d086897b2bfc571aafa0a97dabf
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-12-22 14:21:49 +0000

    Edited constant_parameters

--- client_code/constant_parameters.py
+++ client_code/constant_parameters.py
@@ -8,7 +8,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 # Variables globales que j'appelle par:     constant_parameters.logo_client
-code_app1 = "https://sxgqveyu3c2nj5kr.anvil.app/BBDLB54NWHHYKEE3YYGJGU4G"   # App "AMS Data"  
+code_app1 = "https://sxgqveyu3c2nj5kr.anvil.app/32M6REZ23NPHINCU26GAZBNM"   # App "AMS Data"  
 code_app2 = "https://2erggvosxp234ktt.anvil.app/EK44RJHJVMRZ7R52DY6NEURF" # App "sign-up_for_AMS_Data"
 nom_app_pour_mail = "AMSport Data"                                          
 mon_mail = "jmarc@jmm-formation-et-services.fr"
