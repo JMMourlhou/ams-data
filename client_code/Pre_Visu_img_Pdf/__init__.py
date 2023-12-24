4d57abb99c26b728b0011b073e480388225f2924
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-12-24 16:56:00 +0000

    Edited Generate_BGT_doc_IMG_into_Pdf and Pre_Visu_img_Pdf

--- client_code/Pre_Visu_img_Pdf/__init__.py
+++ client_code/Pre_Visu_img_Pdf/__init__.py
@@ -13,12 +13,9 @@ class Pre_Visu_img_Pdf(Pre_Visu_img_PdfTemplate):
         # Any code you write here will run before the form opens.
         self.image_1.source = file
         self.new_file_name = new_file_name
-        print("new file name:", new_file_name)
-        
-        with anvil.server.no_loading_indicator:
-            self.task = anvil.server.call("run_bg_task_jpg",file, self.new_file_name, stage_num, email, item_requis)
-
-        
+        self.stage_num = stage_num
+        self.email = email
+        self.item_requis = item_requis
 
     def retour_click(self, **event_args):
         """This method is called when the button is clicked"""
@@ -27,13 +24,10 @@ class Pre_Visu_img_Pdf(Pre_Visu_img_PdfTemplate):
 
     def download_click(self, **event_args):
         """This method is called when the button is clicked"""
-        with anvil.server.no_loading_indicator:
-            if self.task.is_completed():
-                # recup file pdf de table temp
-                temp_row = app_tables.temp.search()[0]
-                file = temp_row['media']
-                anvil.media.download(file) 
-                anvil.server.call("task_killer",self.task)
+        # recup file pdf de table temp
+        media_object = anvil.server.call("create_doc_img_into_pdf", self.image_1.source)
+        anvil.media.download(media_object)
+
             
 
 
