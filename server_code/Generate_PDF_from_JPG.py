979c6ebe46e40c1f0ccc41bce16d7800e4b854db
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-12-27 10:20:06 +0000

    Edited Generate_PDF_from_JPG

--- server_code/Generate_PDF_from_JPG.py
+++ server_code/Generate_PDF_from_JPG.py
@@ -3,16 +3,17 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from anvil.pdf import PDFRenderer
-
 """
+         Génération du doc pdf après avoir visualiser l'image jpg correspondante à 'Pre_Visu_img_Pdf_Generation'
+
     quality :
     "original": All images will be embedded at original resolution. Output file can be very large.
     "screen": Low-resolution output similar to the Acrobat Distiller “Screen Optimized” setting.
     "printer": Output similar to the Acrobat Distiller “Print Optimized” setting.
     "prepress": Output similar to Acrobat Distiller “Prepress Optimized” setting.
-    "default": Output intended to be useful across a wide variety of uses, possibly at the expense of a larger output file.
+ ** "default": Output intended to be useful across a wide variety of uses, possibly at the expense of a larger output file.
     """
-# Calculer la taille du file et ajuster la qualité (screen?)
+# Calculer la taille du file et ajuster la qualité (screen : pas assez bon)
 @anvil.server.callable
 def generate_pdf_from_jpg(file, file_name, stage_num, email, item_requis, pr_requis_row):
     pdf_object = PDFRenderer(page_size ='A4',
@@ -20,20 +21,22 @@ def generate_pdf_from_jpg(file, file_name, stage_num, email, item_requis, pr_req
                             landscape = False,
                             margins = {'top': 1.0, 'bottom': 1.0, 'left': 1.0, 'right': 1.0},  #  cm
                             scale = 1.0,
-                            quality =  "default"
+                            quality =  "default"   # 'default' ok pdf 3300 ko --> pdf 368 ko
                             ).render_form('Pre_Visu_img_Pdf_Generation',file, file_name)
-                            #).render_form('Pre_Visu_img_Pdf',file, file_name, stage_num, email, item_requis)
-    # save mediaobject in table pre requi du stage/stagiaire/pre requi  
-    """
-    # finding the row
-    pr_requis_row = app_tables.pre_requis_stagiaire.get(stage_num = stage_num,
-                                              stagiaire_email = email,
-                                              item_requis = item_requis                                             
-                                             )
-    """                                        
-    if pr_requis_row:
+                    
+    # save mediaobject in table pre requi du stage si: doc PDF pas encore existant en table (jpg chargé à l'origine) 
+    #                                                  ou si le nom du doc pdf pas encore formatté (pdf chargé en 1er)
+               
+    media = pr_requis_row['pdf_doc1']   #j'extrai le nom du doc pdf ds la table
+    if media != None:
+        name_media = media.name[0:3]   
+        try:
+            test_if_integer = int(media) # pas d'erreur doc doc pdf a un nom déjà formatté, je ne sauve pas.
+        except:      # si 3 1eres lettres ne sont pas numériques: c'est le doc pdf chargé au départ, et nom d'origine en table, je sauve
+            pr_requis_row.update(pdf_doc1 = pdf_object)
+    else:  # si pas de doc pdf pour ce doc, je sauve
         pr_requis_row.update(pdf_doc1 = pdf_object)
-
+        
     return pdf_object
 
 
