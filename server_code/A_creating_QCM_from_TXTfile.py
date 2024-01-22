47f6004aab70e463ad71e8f7e5784f293ad6b6ee
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-01-22 11:00:24 +0000

    Edited A_creating_QCM_from_TXTfile

--- server_code/A_creating_QCM_from_TXTfile.py
+++ server_code/A_creating_QCM_from_TXTfile.py
@@ -1,20 +1,19 @@
-
 import anvil.files
 from anvil.files import data_files
 
-import anvil.users
+#anvil.users
 import anvil.tables as tables
-import anvil.tables.query as q
+#import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-
+# lecture d'1 fichier txt pour en extraire les données
 @anvil.server.callable
 def file_reading():
     # Read the contents of a file
     cpt=0
     question_txt = ""
-    f = open(data_files['qcm1.txt'])              # se ferme qd ligne est vide
+    f = open(data_files['qcm1.txt'])              # à fermer en sortie qd ligne vide
     x = True
     while x:
         ligne = f.readline()  # lecture  1 ligne
@@ -22,6 +21,9 @@ def file_reading():
             print(f"question # {cpt}, nb de choix: {nb_de_choix-1}, question:{question_txt}")
             print('Fin du fichier')
             break
+        else:
+            dernier_caract = ligne[len(ligne)-1:len(ligne)]
+            print(f"dernier caract: '{dernier_caract}'")
         
         #print(f"lg: {len(ligne)}")
         n = ligne[0:1] 
