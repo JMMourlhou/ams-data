eb73a61d8b016bd99f319efdc0d6dd51a38e7852
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-02-03 17:49:06 +0000

    Created plot; Deleted plot

--- client_code/plot.py
+++ client_code/plot.py
@@ -10,13 +10,13 @@ from anvil.tables import app_tables
 from plotly import graph_objects as go
 
 class plot(plotTemplate):
-    def __init__(self, **properties):
+    def __init__(self, nb=3, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
         # lecture du qcm
-        qcm_n = app_tables.qcm_description.get(qcm_nb=3)
+        qcm_n = app_tables.qcm_description.get(qcm_nb=nb)
         # lecture du stagiaire
         user=anvil.users.get_user()
         if user:
@@ -25,8 +25,8 @@ class plot(plotTemplate):
                                             qcm_number = qcm_n
                                         )
             
-            listx = []
-            listy = []
+            listx = []           # liste nb de fois qcm effectué         (1,    2,   3,  ...)
+            listy = []           # liste du résultat obtenu àch passage  (10%, 25%, 35%, ...)
             list_max = []
             liste_date =[]
             cpt=0
@@ -35,10 +35,11 @@ class plot(plotTemplate):
             for q in qcm_rows:
                 cpt += 1
                 listx.append(cpt)
-                listy.append(q['nb_rep_ok'])
-                max_rep = q['nb_rep_ok']/q['p100_sur_nb_rep']*100
+                listy.append(q['p100_sur_nb_rep'])    
+                max_rep = 100                         # max = 100 %
                 list_max.append(max_rep)
-                liste_date.append(str(q['time'].strftime("%d/%m/%Y")))
+                #liste_date.append(str(q['time'].strftime("%d/%m/%Y")))
+                liste_date.append(str(q['time'].strftime("%d/%m")))
                 
             print(listx)
             print(listy)
@@ -54,17 +55,18 @@ class plot(plotTemplate):
         go.Bar(
             x = listx,
             y = list_max,
-            name = 'max questions'
-        )
+            name = '100 %'
+                )
         ]
     
         # Configure the plot layout
+        title = f"{nb_qcm_passe} Qcm effectués"
         self.plot_1.layout = {
                                 'title': 'Progression des résultats, QCM ' + qcm_n['destination'],
-                                'xaxis': {'title': "Nb d'essais"},
+                                'xaxis': {'title': title},
                                 'tickmode': 1,           # de 1 en 1
                             }
-        self.plot_1.layout.yaxis.title = 'Nb bonnes réponses'
+        self.plot_1.layout.yaxis.title = '% de bonnes réponses'
         
         date_deb = liste_date[0]     #dernière date
         date_fin = liste_date[nb_qcm_passe-1]   # derniere date
@@ -76,7 +78,7 @@ class plot(plotTemplate):
                                                     y = 0,
                                                     yref = 'y',
                                                     showarrow=True,
-                                                    arrowhead=7,
+                                                    arrowhead=15,
                                                     ax=0,
                                                     ay=-40
                                                  ),
@@ -87,7 +89,7 @@ class plot(plotTemplate):
                                                     y = 0,
                                                     yref = 'y',
                                                     showarrow=True,
-                                                    arrowhead=7,
+                                                    arrowhead=15,
                                                     ax=0,
                                                     ay=-40
                                                  )
@@ -97,17 +99,4 @@ class plot(plotTemplate):
         """This method is called when the button is clicked"""
         from ..Main import Main
         open_form('Main',99)
-"""
-'tickmode': 'array',
 
-# Specify the layout
-    layout = {
-      'title': 'Election results',
-      'yaxis': {'title': 'Seats'},
-      'xaxis': {
-        'tickmode': 'array',
-        'tickvals': list(range(27)),
-        'ticktext': data['year'],
-      },
-    }
-"""
