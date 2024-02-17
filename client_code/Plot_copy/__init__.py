3095d10e2864afb67a733b3bf4a6aa8a77d7c73f
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-02-17 17:00:23 +0000

    Created Plot_copy; Edited Plot

--- client_code/Plot_copy/__init__.py
+++ client_code/Plot_copy/__init__.py
@@ -1,5 +1,8 @@
-from ._anvil_designer import PlotTemplate
+from ._anvil_designer import Plot_copyTemplate
 from anvil import *
+import stripe.checkout
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import plotly.graph_objects as go
 import anvil.server
 import anvil.users
@@ -9,61 +12,67 @@ from anvil.tables import app_tables
 
 from plotly import graph_objects as go           # AFFICHAGE DES RESULTATS de plusieurs tests sur 1 QCM
 
-class Plot(PlotTemplate):
+class Plot_copy(Plot_copyTemplate):
     def __init__(self,user, nb, legend = False, **properties):        # le nb vient de temp3 ds user (à cause du qcm BNSSA tiré de plusieurs qcm)
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
-        # lecture du qcm description 
+        print("***************** user:", user["email"])
+        # lecture du qcm
         qcm_n = app_tables.qcm_description.get(qcm_nb=nb)
-        min_rep = qcm_n['taux_success']                      # INT       ex : min = 75 %
-        
-        # lecture table qcm result, création de la liste des qcm de ce stagiaire pour 1 QCM
-        listx_int = []           # liste nb de fois qcm effectué         (1,    2,   3,  ...)
-        listx_str = []           # liste nb de fois, mais en str pour affichage de x
-        listy = []           # liste du résultat obtenu àch passage  (10%, 25%, 35%, ...)
-        list_min = []
-        liste_date =[]
-        cpt=0
+        """
+        # lecture du stagiaire
+        user=anvil.users.get_user()
+        if not user:
+            print("pas de user")
+            return
+        """
+
         if user:
-            qcm_rows = app_tables.qcm_result.search(                         # permet la boucle sur les qcm passés
+            qcm_rows = app_tables.qcm_result.search(
                                             user_qcm = user,
                                             qcm_number = qcm_n
-                                        )       
+                                        )
+
+
+            listx_int = []           # liste nb de fois qcm effectué         (1,    2,   3,  ...)
+            listx_str = []           # liste nb de fois, mais en str pour affichage de x
+            listy = []           # liste du résultat obtenu àch passage  (10%, 25%, 35%, ...)
+            list_min = []
+            liste_date =[]
+            cpt=0
             nb_qcm_passe = len(qcm_rows)
-        else:
-            print("plot: user non trouvé")
-            return
-            
-        for q in qcm_rows:
-            cpt += 1
-            listx_int.append(cpt)                   # x INT
-            listx_str.append(str(cpt))              # x STR
-            listy.append(q['p100_sur_nb_rep'])      # y INT  Résultat du qcm 
-            list_min.append(min_rep)                # INT       ex : min = 75 %    pour dessiner la ligne horizontale du mini recqui
-            if len(qcm_rows)>1:                     # s'il y a au moins 1 qcm passé
-                liste_date.append(str(q['time'].strftime("%d/%m")))
-            else:
-                liste_date.append(str(q['time'].strftime("%d/%m/%Y, %Hh%M")))            # 1 seul qcm, j'affiche les détails du timing
-        
-        print('date / h : ', str(liste_date[nb_qcm_passe-1]))
-        print("x int  ",listx_int)
-        print("x text ",listx_str)
-        print("y", listy)
-        print(list_min)
-                
+
+            for q in qcm_rows:
+                cpt += 1
+                listx_int.append(cpt)
+                listx_str.append(str(cpt))
+                listy.append(q['p100_sur_nb_rep'])
+                min_rep = 75                         # max = 75 %
+                list_min.append(min_rep)
+                if len(qcm_rows)>1:
+                    liste_date.append(str(q['time'].strftime("%d/%m")))
+                else:
+                    liste_date.append(str(q['time'].strftime("%d/%m/%Y, %Hh%M")))
+
+            print('date / h : ', str(liste_date[nb_qcm_passe-1]))
+            print("x int  ",listx_int)
+            print("x text ",listx_str)
+            print("y", listy)
+            print(list_min)
+
         # Plot some data     SI PLUSIEURS QCM EFFECTUES j'affiche la ligne de 75%
         if len(qcm_rows)>1:
             self.label_nom.text = f"{user['email']}"
-            title = f"{nb_qcm_passe} tests pour QCM '{qcm_n['destination']}'"
+            title = f"{nb_qcm_passe} passages pour le QCM '{qcm_n['destination']}''"
             self.plot_1.data = [
                 go.Scatter(
                         x = listx_int,
                         y = listy,
                         marker = dict(color= 'rgb(16, 32, 77)' )
                     ),
-                go.Scatter(                                        # la ligne du mini recquis
+                go.Scatter(
                         x = listx_int,
                         y = list_min,
                         marker = dict(color= 'rgb(204, 0, 0)' ),
@@ -83,7 +92,7 @@ class Plot(PlotTemplate):
             else:
                 self.label_nom.text = f"Echec au QCM {qcm_n['destination']} / {user['nom']} {user['prenom']}"
                 title_pie = "Echec"
-            
+
             self.plot_1.data = [
                 go.Pie(
                         values=listy_pour1qcm,
@@ -93,18 +102,18 @@ class Plot(PlotTemplate):
                         marker_colors=colors
                     )
             ]
-          
+
         # Configure the plot layout
-        if len(qcm_rows)>1:     # Plusieurs passages
+        if len(qcm_rows)>1:
             self.plot_1.layout = {  'displayModeBar' : True,          # False: n'affiche que qd passe au dessus
                                     'modeBarButtonsToRemove' : ['zoomIn2d', 'zoomOut2d', 'pan2d' ],
                                     'title': title,
                                     'xaxis': {'title': title,
                                             'visible': False                 # Masque l'axe X et son titre  !!
                                             },
-                                    'yaxis': dict(range=[0, 100]),  
+                                    'yaxis': dict(range=[0, 100]),
                                     'yaxis': {'title': title,
-                                            'visible': True               # Masque l'axe y et son titre  !!
+                                            'visible': False                # Masque l'axe y et son titre  !!
                                             },
                                     'plot_bgcolor': 'lightblue',       # Couleur de fond personnalisée
                                     #'tickmode': 'array',               # de 1 en 1
@@ -124,9 +133,9 @@ class Plot(PlotTemplate):
                                             },
                                     'plot_bgcolor': 'lightblue',       # Couleur de fond personnalisée
                                 }
-            
+
         self.plot_1.layout.yaxis.title = '% réponses ok - ' + user['email']
-        
+
         date_deb = liste_date[0]     #dernière date
         date_fin = liste_date[nb_qcm_passe-1]   # derniere date
         if len(qcm_rows)>1:
@@ -140,7 +149,7 @@ class Plot(PlotTemplate):
                                                         showarrow=True,       # Montre lea flèche :True
                                                         arrowhead=15,
                                                         ax=0,
-                                                        ay=-20, 
+                                                        ay=-20,
                                                         color = 'white',
                                                         fontsize = 12
                                                     ),
@@ -158,8 +167,8 @@ class Plot(PlotTemplate):
                                                         fontsize = 12
                                                     )
                                             ]
-      
-            
+
+
     def button_annuler_click(self, **event_args):
         """This method is called when the button is clicked"""
         from .Main import Main
@@ -169,4 +178,3 @@ class Plot(PlotTemplate):
         """This method is called when a data point is clicked."""
         print("download")
         #anvil.media.download(self.plot_1)
-
