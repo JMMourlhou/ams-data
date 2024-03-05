c2ac705aa8b2fa7711293508516c863354f69aff
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-03-05 21:15:39 +0000

    Edited QCM_Results.ItemTemplate7.ItemTemplate8

--- client_code/QCM_Results/ItemTemplate7/ItemTemplate8/__init__.py
+++ client_code/QCM_Results/ItemTemplate7/ItemTemplate8/__init__.py
@@ -16,9 +16,23 @@ class ItemTemplate8(ItemTemplate8Template):
 
         # Any code you write here will run before the form opens.
         self.button_qcm_descro.text = self.item['qcm_number']['destination']
-        self.button_qcm_time.text = self.item['time'].strftime("%d/%m/%Y, %Hh%M")
+        self.button_qcm_time.text = "le " + str(self.item['time'].strftime("%d/%m/%Y")) + " à " + str(self.item['time'].strftime("%Hh%M")) 
         if self.item['success'] == True:
             self.button_qcm_result.background = "green"
+            self.button_qcm_time.background = "green"
         else:
             self.button_qcm_result.background = "red"
-        self.button_qcm_result.text = self.item['p100_sur_nb_rep'] + " %"
+            self.button_qcm_time.background = "red"
+        self.button_qcm_result.text = str(self.item['p100_sur_nb_rep']) + " %"
+
+    def button_qcm_descro_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+    def button_qcm_result_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+    def button_qcm_time_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
