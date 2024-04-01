98e606b50820559441f57db22dd72eec596be80d
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-04-01 20:39:12 +0000

    Created _q_fetch_exemple; Edited French_zone_server_side, QCM_Results.ItemTemplate7, QCM_Results.ItemTemplate7.ItemTemplate8, QCM_visu_modif_Main and 7 more; Deleted Form1

--- client_code/_q_fetch_exemple/__init__.py
+++ client_code/_q_fetch_exemple/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import _q_fetch_exempleTemplate
 from anvil import *
 import anvil.server
 import stripe.checkout
@@ -9,7 +9,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class _q_fetch_exemple(_q_fetch_exempleTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
--- client_code/test_qcm_modif.py
+++ client_code/test_qcm_modif.py
@@ -1,7 +1,6 @@
-import stripe.checkout
+
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
+
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
