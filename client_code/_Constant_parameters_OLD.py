d1c382537fe66d25924a564e9b769cfdcadd512c
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-04-11 16:19:08 +0000

    Created _Constant_parameters_OLD and Variables_globales; Edited QrCode_display, settings and Visu_stages; Deleted Visu_liste_1_stage.ItemTemplate2, constant_parameters and Visu_liste_1_stage

--- client_code/_Constant_parameters_OLD.py
+++ client_code/_Constant_parameters_OLD.py
@@ -1,12 +1,4 @@
 
-#import anvil.server
-#import anvil.google.auth, anvil.google.drive
-#from anvil.google.drive import app_files
-#import anvil.users
-#import anvil.tables as tables
-#import anvil.tables.query as q
-#from anvil.tables import app_tables
-
 # Variables globales que j'appelle par:     constant_parameters.logo_client
 code_app1 = "https://sxgqveyu3c2nj5kr.anvil.app/32M6REZ23NPHINCU26GAZBNM"   # App "AMS Data"  
 code_app2 = "https://2erggvosxp234ktt.anvil.app/EK44RJHJVMRZ7R52DY6NEURF" # App "sign-up_for_AMS_Data"
