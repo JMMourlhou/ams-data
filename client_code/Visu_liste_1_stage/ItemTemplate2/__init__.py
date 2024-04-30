e65fe344a7428ea074aff611fd1e0cdf0f14b583
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-04-30 16:40:57 +0000

    Created Visu_liste_1_stage.ItemTemplate2 and Visu_liste_1_stage; Deleted Form1

---
+++ client_code/Visu_liste_1_stage/ItemTemplate2/__init__.py
@@ -0,0 +1,62 @@
+from ._anvil_designer import ItemTemplate2Template
+from anvil import *
+import stripe.checkout
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from anvil_extras.PageBreak import PageBreak
+from ... import constant_parameters
+
+global cpt  # compteur des lignes
+cpt = 0
+
+class ItemTemplate2(ItemTemplate2Template):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+        
+        #lecture fichier users à partir du mail
+        mel=self.item["user_email"]['email']
+        stagiaire = app_tables.users.get(email=mel)    
+        if stagiaire :
+            #Photo
+            orig_pic = stagiaire['photo']
+            if orig_pic != None:
+                thumb_pic = anvil.image.generate_thumbnail(orig_pic, 320)
+                self.image_1.source = thumb_pic
+            
+            # self.text_box_5.text
+            fi = self.item["financement"]["code_fi"]
+            finance = app_tables.mode_financement.get(code_fi=fi)
+            #self.text_box_5.text = finance['intitule_fi']
+
+            self.rich_text_1.border="0px solid blue"
+            self.rich_text_1.font_size=17
+            self.rich_text_1.bold=False
+            self.rich_text_1.italic=False
+            self.rich_text_1.align="center"
+            self.rich_text_1.font="Noto"
+            date_naiss_format = stagiaire['date_naissance'].strftime("%d/%m/%Y")
+            #self.rich_text_1.background="theme:Primary"
+            #self.rich_text_1.foreground="theme:On Primary"
+            self.rich_text_1.content=f" **{stagiaire['nom']} {stagiaire['prenom']}** ({finance['code_fi']}) \n{stagiaire['email']} \n {stagiaire['tel']} "
+            self.rich_text_2.content=f" Né le {date_naiss_format} à {stagiaire['ville_naissance']} ({stagiaire['code_postal_naissance']} {stagiaire['pays_naissance']}) \n {stagiaire['adresse_rue']}, {stagiaire['adresse_code_postal']} {stagiaire['adresse_ville']} "
+
+
+
+    def image_1_show(self, **event_args):
+        """This method is called when the Image is shown on the screen"""
+    
+        global cpt  # Cpt le nb de form imprimée
+        cpt += 1
+        #récup du param 
+        nb_stag_par_page = constant_parameters.nb_fiche_stagiaire_pdf
+        if (cpt // nb_stag_par_page) * nb_stag_par_page == cpt:          # ts les 1 ou 5 stagiaires, selon param global
+           self.add_component(PageBreak())      # si en création de pdf, je saute une page ts les n stagiares 
+ 
---
+++ client_code/Visu_liste_1_stage/__init__.py
@@ -0,0 +1,71 @@
+from ._anvil_designer import Visu_liste_1_stageTemplate
+from anvil import *
+import stripe.checkout
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.media                               # pour le pdf standard
+
+global pdf_mode
+pdf_mode=False
+
+class Visu_liste_1_stage(Visu_liste_1_stageTemplate):
+    def __init__(self, num_stage, intitule, pdf_mode=False, **properties):    #si pdf_mode=True ouverture pour pdf
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        # Any code you write here will run before the form opens.
+        self.num_stage = num_stage
+        self.intitule = intitule
+        self.pdf_mode = pdf_mode
+        if self.pdf_mode == True:
+            self.button_annuler.visible = False
+            self.button_pdf.visible = False
+            self.button_trombi.visible = False
+        
+        #lecture du fichier père stages
+        stage_row = app_tables.stages.get(numero=int(num_stage))    
+        self.repeating_panel_1.items = app_tables.stagiaires_inscrits.search(
+            tables.order_by("name", ascending=True),
+            stage=stage_row
+        )
+        """ Je peux créer une liste à partir de l'objet créé par search ( avec list() )
+             et accéder ensuite à chaque row et column:
+             
+        list1=list(app_tables.stagiaires_inscrits.search(
+            tables.order_by("name", ascending=True),
+            stage=stage_row
+        ))
+        
+        print(list1)
+        print(list1[0]['name'])     row 1, column 'nom'
+        """
+
+        cod = stage_row["code"]['code']
+        date = str(stage_row["date_debut"].strftime("%d/%m/%Y"))
+        self.label_titre.text = "Fiches stagiaires " + cod + " du " + date + "   (num " +num_stage+")"
+
+    def button_annuler_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        from ..Visu_stages import Visu_stages
+        open_form('Visu_stages')
+
+    def button_pdf_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        with anvil.server.no_loading_indicator:
+            media_object = anvil.server.call('run_bg_task',self.num_stage, self.intitule)
+        "lecture du media object que j'ai stocké en server module"
+        stage_row = app_tables.stages.get(numero=int(self.num_stage))
+        if not stage_row:   
+            print("stage non trouvé à partir de num_stages server module: Stagiaires_list_pdf")
+        else:
+            anvil.media.download(stage_row["list_media"])
+
+    def button_trombi_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        from ..Visu_trombi import Visu_trombi
+        open_form('Visu_trombi',self.num_stage, self.intitule)
+
