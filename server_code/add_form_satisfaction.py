71ba33cd0d123b7a456923959e00fb4c6aec5c43
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-05-16 16:59:32 +0000

    Edited add_form_satisfaction and settings

--- server_code/add_form_satisfaction.py
+++ server_code/add_form_satisfaction.py
@@ -23,4 +23,18 @@ def add_1_formulaire_satisfaction(  user_stagiaire,
     print(dico_rep_q_ouv)
     print()
     print(date_time)
-    return(False)
+    new_row=app_tables.stage_satisf.add_row(stage_row=stage_row,
+                                    stage_type_txt=stage_row["code_txt"],
+                                    stage_num_txt=str(stage_row["numero"]),
+                                    date_heure=date_time,
+                                    rep_dico_rep_ferm=dico_rep_q_ferm,
+                                    rep_dico_rep_ouv=dico_rep_q_ouv
+                                   )
+    #relecture du row:
+    re_read_row = app_tables.stage_satisf.get(stage_row=new_row["stage_row"],
+                                            date_heure=new_row["date_heure"],
+                                            rep_dico_rep_ferm=new_row["rep_dico_rep_ouv"])
+    if re_read_row:                        
+        return(True)
+    else:
+        return(False)
