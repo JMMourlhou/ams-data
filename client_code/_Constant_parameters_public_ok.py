e254692bbf6fbebabca70c743addd2a46c22c2c2
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-05-31 22:13:57 +0000

    Created Logo_AMS.png; Edited _Constant_parameters_public_ok, Main and Mail_sending

--- client_code/_Constant_parameters_public_ok.py
+++ client_code/_Constant_parameters_public_ok.py
@@ -2,3 +2,4 @@ import anvil.server
 
 nb_fiche_stagiaire_pdf = 5  # 1 ou 5   (1 fiche avec photo en haut ou 5/page)
 mon_logo = "logo_fs_small.png"
+ams_logo = "Logo_AMS.png"
--- server_code/Mail_sending.py
+++ server_code/Mail_sending.py
@@ -14,7 +14,7 @@ from anvil.http import url_encode
 #import sys
 from . import French_zone_server_side
 from . import Variables_globales # importation du module de lecture des variables globales (sauf mon logo)
-#from . import var_globales # importation du module de lecture de mon logo
+from . import _Constant_parameters_public_ok
 
 """Send an email to the specified user"""
 @anvil.server.callable
@@ -25,18 +25,23 @@ def send_mail(user_row, subject_txt, rich_text="Rich_text à insérer"):
         
     client_mail = dict_var_glob["client_mail"]   # var globalenMail ams
     code_app2 = dict_var_glob["code_app2"]      # var_globale de l'apli AMS DATA
-    logo = dict_var_glob["logo_client"]         # var_globale du logo AMSport
-    logo_address = code_app2+logo
+    logo = app_tables.files.get(path="Logo_F_S_small.png")
+    #logo = dict_var_glob["logo_client"]         # var_globale du logo AMSport
+    #logo_address = code_app2+logo
+    #logo_address = code_app1+"/_/theme/"+logo['path']
+    logo_address = code_app2+"/_/theme/"+_Constant_parameters_public_ok.ams_logo
+    print(logo_address)
     
     anvil.email.send(
         to=user_row['email'],
         subject=subject_txt,
         html=f"""
-            <p><img src = {logo_address} width="200" height="200"> </p> 
+            <p><img src = {logo_address} width="200" height="100"> </p> 
             <b> {user_row["prenom"]},</b><br>
             <br>
             <b>{rich_text}</b>
-            <br><br>
+             <br>
+            <b>  https://sxgqveyu3c2nj5kr.anvil.app/32M6REZ23NPHINCU26GAZBNM   </b><br><br>
             <b><i>         L'équipe d'AMSport,</i></b>
             <br>
             <b>{client_mail}</b> <br>
