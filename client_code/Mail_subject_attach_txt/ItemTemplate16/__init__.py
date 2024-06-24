4ed48c04992da26bd2592d5c07e1b8304efcf6f6
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-06-24 18:07:09 +0000

    Created Display_pdf_file; Edited Mail_subject_attach_txt.ItemTemplate16; Deleted Pdf_display

--- client_code/Mail_subject_attach_txt/ItemTemplate16/__init__.py
+++ client_code/Mail_subject_attach_txt/ItemTemplate16/__init__.py
@@ -13,12 +13,20 @@ class ItemTemplate16(ItemTemplate16Template):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
-        self.image_doc.source = self.item[0]        # 1er élément de l'item
-        self.label_address_doc.text = self.item[1]  # 2eme élément de l'item
+        #self.image_doc.source = self.item[0]        # 1er élément de l'item, le media file choisit pour téléchargement (si pdf, le transfomer en jpg)
+        self.label_address_doc.text = self.item[1]  # 2eme élément de l'item, le nom du fichier en txt
         
-        url = anvil.server.call('get_pdf_url', self.item[0]) 
-        print(url)
-        self.image_doc.source =   anvil.js.call_js('show_pdf',url)
+        # Type de fichier en attachement ?
+        path_parent, file_name, file_extension = anvil.server.call('path_info', str(self.item[0].name))
+        if file_extension == ".pdf":
+            liste_images = anvil.server.call('display_pdf', self.item[0]) 
+        
+            #extraction 1ere image de la liste (il peut y avoir plusieurs pages)
+            print("nb d'images jpg crées par pdf_into_jpg:", len(liste_images))
+            file = liste_images[0]
+            self.image_doc.source =  file
+        else:
+            self.image_doc.source = self.item[0]        # 1er élément de l'item, le media file choisit pour téléchargement (si pdf, le transfomer en jpg)
         
     def button_del_click(self, **event_args):
         """This method is called when the button is clicked"""
--- server_code/Pdf_display.py
+++
@@ -1,5 +0,0 @@
-import anvil.server
-
-@anvil.server.callable
-def get_pdf_url(file):
-    return file.get_url()
