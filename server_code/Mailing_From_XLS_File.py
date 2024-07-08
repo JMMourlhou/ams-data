db246d83211b370730028f62969e2d24512069ed
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-07-08 11:37:37 +0000

    Edited Mailing_From_XLS_File and settings

--- server_code/Mailing_From_XLS_File.py
+++ server_code/Mailing_From_XLS_File.py
@@ -20,18 +20,38 @@ def xls_file_reader(csv_file_name):
             print('Fin du fichier')
             break
         
-        #remplacement ; par , 
+        # création d'une liste, à prtir du séparateur ";"
         ligne = text.split(';')
-        # extraction du mail
-        mail=ligne[1]
-        #extraction du num
-        num=ligne[0]
-        app_tables.stagiaires_histo.add_row(mail=mail,
-                                            num=num)
-    
-
-
-
+        
+        num=ligne[0]           # extraction du num ligne excel
+        mail=ligne[1]          # extraction du mail
+        diplome=ligne[2]       # extraction du diplome
+        lieu_diplome=ligne[3]  # extraction du lieu diplome
+        date_diplome=ligne[4]  # extraction date du diplome
+        nom=ligne[5]           # extraction du nom
+        prenom=ligne[6]        # extraction du prenom
+        date_naissance=ligne[7]  # extraction date N
+        lieu_naissance=ligne[8]  # extraction lieu N
+        rue=ligne[9]             # extraction rue
+        cp=ligne[10]             # extraction cp
+        ville=ligne[11]          # extraction ville
+        tel=ligne[12]            # extraction du tel
+       
+        if lieu_diplome == "CARNON":
+            app_tables.stagiaires_histo.add_row(mail=mail,
+                                                num=num,
+                                                diplome=diplome,
+                                                lieu_diplome=lieu_diplome,
+                                                date_diplome=date_diplome,
+                                                nom=nom,
+                                                prenom=prenom,
+                                                date_n=date_naissance,
+                                                lieu_n=lieu_naissance,
+                                                rue=rue,
+                                                ville=ville,
+                                                cp=cp,
+                                                tel=tel
+                                            )
     """
     wb = openpyxl.load_workbook(f)
     sheet = wb[wb.sheetnames[0]]
