365c345d4148d7c79d69ba41220faf98f247b555
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-07-24 10:17:11 +0000

    Created Stage_form_satisf_suivi; Edited stage_creation_modif_del and settings

--- server_code/stage_creation_modif_del.py
+++ server_code/stage_creation_modif_del.py
@@ -35,8 +35,10 @@ def add_stage(code_stage,     # row codes_stage concernée
                               commentaires = commentaires,
                               allow_bgt_generation = False,
                               saisie_satisf_ok = False,                                 # Ne pas saisir le form de stisfaction            
-                              satis_dico1_q_ferm=code_stage["satisf_q_ferm_template"],  # copie du template de la table "code_stages", questions fermées
-                              satis_dico2_q_ouv=code_stage["satisf_q_ouv_template"]     # copie du template de la table "code_stages", questions ouvertes
+                              satis_dico1_q_ferm=code_stage["satisf_q_ferm_template"],  # copie du template satisf de la table "code_stages", questions fermées
+                              satis_dico2_q_ouv=code_stage["satisf_q_ouv_template"],     # copie du template satisf de la table "code_stages", questions ouvertes
+                              suivi_dico1_q_ferm=code_stage["suivi_stage_q_ferm_template"],  # copie du template suivi de la table "code_stages", questions fermées
+                              suivi_dico2_q_ouv=code_stage["suivi_stage_q_ouv_template"]    # copie du template suivi de la table "code_stages", questions ouvertes
                              )
         
                  
