1b11c84e1c9f3a3cb16548f909bd6a532c97d52a
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-07-31 14:49:34 +0000

    Edited Mail_sending

--- server_code/Mail_sending.py
+++ server_code/Mail_sending.py
@@ -67,8 +67,6 @@ def send_mail(emails_list, subject_txt, rich_text, attachments=[], old_stagiaire
                 row_old_stagiaire = app_tables.stagiaires_histo.get_by_id(id)
                 row_old_stagiaire.update(envoi=False, erreur_mail=True)
                 
-            
         # possible de changer la couleur d'un texte:   <b><p style="color:blue;"> {user_row["prenom"]}, </p></b>
-        # insertion du logo     <p><img src = {en_tete_address} width="150" height="75"> </p>
     return True, nb_mails
 
