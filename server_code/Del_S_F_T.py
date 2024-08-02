8e050452099586b7790ecfb78c30cdad8edf0149
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-08-02 19:02:23 +0000

    Edited Del_S_F_T

--- server_code/Del_S_F_T.py
+++ server_code/Del_S_F_T.py
@@ -2,8 +2,7 @@
 import anvil.files
 from anvil.files import data_files
 import anvil.email
-import anvil.google.auth, anvil.google.drive, anvil.google.mail
-from anvil.google.drive import app_files
+
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -21,30 +20,4 @@ def del_personne(row):
     if not row1:
         msg = "Effacement effectué !"
     return msg
-    """                                                       Module qui fonctionne mais trop lent
-    list = app_tables.users.search()   
-    for row in list:
-        if row["email"] == personne_row['email']:
-            row.delete()
-            break
-    row1 = app_tables.users.get(email=personne_row['email'])
-    if not row1:
-        msg = "Effacement effectué !"
-    return msg
-    """
-
     
-    """
-    print(personne_row)
-    print(personne_row['email'])
-    msg = "Erreur en effacement"
-    row_users = app_tables.users.get(email=personne_row['email'])
-    print(row_users)
-    print(row_users['email'])
-    if row_users:
-        row_users.delele()
-        row = app_tables.users.get(email=personne_row)
-        if not row:
-            msg = "Effacement effectué !"
-    return msg
-    """
