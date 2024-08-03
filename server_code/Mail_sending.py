6664eae9aeb2a67a96e93691d25df7c0331c319c
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-08-03 16:27:23 +0000

    Edited settings and Mail_sending

--- server_code/Mail_sending.py
+++ server_code/Mail_sending.py
@@ -19,8 +19,7 @@ from . import _Constant_parameters_public_ok
 """
 @anvil.server.background_task
 def send_mail(emails_list, subject_txt, rich_text, old_stagiaires=False, attachments=[]):
-    # recup de date heure de l'envoi
-    time=French_zone_server_side.time_french_zone() # time is a datetime format 
+    
     # Initialisation du nb de mails envoyés
     nb_mails = 0
     # Récupération des variables globales
@@ -34,11 +33,12 @@ def send_mail(emails_list, subject_txt, rich_text, old_stagiaires=False, attachm
     fin_mail_carte = code_app2+"/_/theme/"+_Constant_parameters_public_ok.carte_ams
     fin_mail_qualiopi = code_app2+"/_/theme/"+_Constant_parameters_public_ok.logo_qualiopi
     # ------------------------------------------
-    print("logo address ok: ",logo_address)
-    
-    print(emails_list)
+    # Tps début de traitement
+    time_deb=French_zone_server_side.time_french_zone() # time is a datetime format 
     for email, prenom, id in emails_list:
-        print()
+        # time début de traitement
+        time=French_zone_server_side.time_french_zone() # time is a datetime format 
+        print(time)
         try:
             anvil.email.send(
                 to=email,
@@ -60,7 +60,6 @@ def send_mail(emails_list, subject_txt, rich_text, old_stagiaires=False, attachm
             # sauve le nb de mails ds table temp
             table_temp = app_tables.temp.search()[0]
             table_temp.update(nb_mails_sent=nb_mails)
-            print("old_stgiaires 2",old_stagiaires)
             
             if old_stagiaires is True:
                 # sauver la date et l'heure
@@ -75,11 +74,10 @@ def send_mail(emails_list, subject_txt, rich_text, old_stagiaires=False, attachm
             fichiers_txt = ""
             if attachments != []:
                 for file in attachments:
-                    print(file)
                     fichiers_txt = fichiers_txt + str(file.name) +" , "
                 
             app_tables.mails_histo.add_row(
-                                            date_heure=time,
+                                            date_heure=time, # time is a datetime format ,
                                             mail=email,
                                             objet=subject_txt,
                                             fichiers_attachés=fichiers_txt
@@ -92,8 +90,14 @@ def send_mail(emails_list, subject_txt, rich_text, old_stagiaires=False, attachm
                 row_old_stagiaire.update(envoi=False, erreur_mail=True)
                 
         # possible de changer la couleur d'un texte:   <b><p style="color:blue;"> {user_row["prenom"]}, </p></b>
-
     
+    # Tps fin de traitement
+    time_fin=French_zone_server_side.time_french_zone() # time is a datetime format 
+    # Durée de traitement
+    time_traitement = time_fin - time_deb
+    print("durée du traitement: ", time_traitement)
+
+
 @anvil.server.callable
 def run_bg_task_mail(emails_list, subject_txt, rich_text, attachments=[], old_stagiaires=False):
     print("old_stagiaires 1",old_stagiaires)
