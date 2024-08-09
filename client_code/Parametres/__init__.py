5cea5497a675b96f7e79d81231f165399c93a6d7
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-08-09 18:18:16 +0000

    Edited Parametres

--- client_code/Parametres/__init__.py
+++ client_code/Parametres/__init__.py
@@ -1,20 +1,15 @@
 from ._anvil_designer import ParametresTemplate
 from anvil import *
 import anvil.server
-import stripe.checkout
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Parametres(ParametresTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
         # Any code you write here will run before the form opens.
 
     def button_maj_pr_click(self, **event_args):
@@ -35,16 +30,10 @@ class Parametres(ParametresTemplate):
     def drop_down_code_stage_change(self, **event_args):
         """This method is called when an item is selected"""
         self.stage_row = self.drop_down_code_stage.selected_value
-        self.drop_down_code_personne.items = [(r["name"]+" "+r["prenom"], r) for r in app_tables.stagiaires_inscrits.search(
-                                                                                                                                tables.order_by("name", ascending=True),
-                                                                                                                                stage_txt=self.stage_row["code"]
-                                                                                                                                )
-                                            ]
-        
-
+        self.drop_down_personnes.items = [(r["name"]+" "+r["prenom"], r) for r in app_tables.stagiaires_inscrits.search()]
 
     def button_gestion_pre_requis_personnel_click(self, **event_args):
         """This method is called when the button is clicked"""
-        # INITIALISATION Drop down des types de stage
-        self.drop_down_code_stage.items = [(r["code"], r) for r in app_tables.codes_stages.search(tables.order_by("code", ascending=True))]
-        self.column_panel_pr_par_personne.visible = True
+        pass
+
+ 
