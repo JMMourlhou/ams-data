61bc75a314aa5f3ba70b2c0087f70b3d3cc7b0d5
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-10-16 16:43:01 +0000

    Edited Recherche_stagiaire

--- client_code/Recherche_stagiaire/__init__.py
+++ client_code/Recherche_stagiaire/__init__.py
@@ -223,6 +223,9 @@ class Recherche_stagiaire(Recherche_stagiaireTemplate):
     def button_insc_par_qr_click(self, **event_args):
         """This method is called when the button is clicked"""
         # si True, appel du qr_code pour que les stagiaires log in ds l'appli
-        open_form('QrCode_display',False, str(self.selection['numero']))
+        if self.selection['numero'] != 1003:
+            open_form('QrCode_display',False, str(self.selection['numero']))
+        else:
+            pass
 
 
