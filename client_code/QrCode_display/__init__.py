3d9e77084ab5eeb884aab9bb0b4e77165ec3ca5b
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-10-17 20:07:15 +0000

    Edited QrCode_display

--- client_code/QrCode_display/__init__.py
+++ client_code/QrCode_display/__init__.py
@@ -34,6 +34,8 @@ class QrCode_display(QrCode_displayTemplate):
             if not stage:
                 alert("Code du stage non trouvé")
                 return
+            txt_stage=stage['code']['code']
+            txt_stage=txt_stage.replace("_","")
 
             # si num_stage="1003", stage tuteur, il faut savoir pour quel stage lestuteurs seront inscrits
             # l'info sera contenue ds temp_pour_stage ds la table user puis ds table stagiaire inscrit)
@@ -46,31 +48,10 @@ class QrCode_display(QrCode_displayTemplate):
                 self.drop_down_num_stages.items = [(r['code_txt']+" / "+str(r['date_debut'])+" / "+str(r['numero']), r) for r in list]
                 self.column_panel_choix_stage.visible = True
                 self.text_area_lien.visible = False
-            
-            
-            txt_stage=stage['code']['code']
-            txt_stage=txt_stage.replace("_","")
-            self.label_titre.text = "Flachez pour s'inscrire au "+ txt_stage + " du " + str(stage['date_debut'].strftime("%d/%m/%Y"))
-           
-            
-            time = self.recup_time()
-            param="/#?a=qrcode" + "&stage=" + str(num_stage) + "&t=" + time
-        
-        
-
-
-
-        # CREER UNE FONCTION pour tous stages 1003 et tous les autres
-        
-        # Lecture de la variable globale "code_app1" ds table variables_globales
-        app = anvil.server.call('get_variable_value', "code_app1")
-        
-        stage_link = app + param  # App "AMS Data"  + code stage
-        self.text_area_lien.text = stage_link
-        print(stage_link)
-        self.text_area_lien.display = True
-        media=anvil.server.call('mk_qr_code',stage_link)
-        self.image_1.source=media
+                self.label_titre.text = "Choisir quel stage encadre le tuteur"
+            else:
+                self.label_titre.text = "Flachez pour s'inscrire au "+ txt_stage + " du " + str(stage['date_debut'].strftime("%d/%m/%Y"))
+                self.link_creation(1003, 0)        
 
     def recup_time(self, **event_args): 
         time=French_zone.french_zone_time()
@@ -84,11 +65,33 @@ class QrCode_display(QrCode_displayTemplate):
         from ..Main import Main
         open_form('Main',99)
 
+    # si stage 1003 pour tuteurs 
     def drop_down_num_stages_change(self, **event_args):
         """This method is called when an item is selected"""
         pour_stage_row = self.drop_down_num_stages.selected_value
-        self.pour_stage = pour_stage_row['numero']
+        pour_stage_num = pour_stage_row['numero']
         self.text_area_lien.visible = False
+        self.label_titre.text = "Flachez pour inscrire les tuteurs du stage "+ pour_stage_row['code_txt'] + " du " + str(pour_stage_row['date_debut'].strftime("%d/%m/%Y"))
+        self.link_creation(1003, pour_stage_num)
+    
+    def link_creation(self, num_stage, pour_stage=0):
+        # Lecture de la variable globale "code_app1" ds table variables_globales
+        app = anvil.server.call('get_variable_value', "code_app1")
+        
+        # param 
+        time = self.recup_time()
+        param="/#?a=qrcode" + "&stage=" + str(num_stage) +  "&pour=" +  str(pour_stage)  + "&t=" + time
+        #param="/#?a=qrcode" + "&stage=" + str(num_stage) + "&t=" + time
+
+        stage_link = app + param  # App "AMS Data"  + code stage
+        
+        self.text_area_lien.text = stage_link  # affichage du lien
+        self.text_area_lien.visible = True
+        
+        media=anvil.server.call('mk_qr_code',stage_link) # affichage du QR code
+        self.image_1.source=media
+
+        
         
 
         
