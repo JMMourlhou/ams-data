e3d2a168e50136a4a66d8ebf0698b74f417dd16f
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-10-18 17:28:55 +0000

    Edited Main

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -172,7 +172,8 @@ class Main(MainTemplate):
         """
         self.content_panel.clear()
         alert("envoi en SignupDialog_V2")
-        self.content_panel.add_component(SignupDialog_V2(h, num_stage, pour_stage), full_width_row=True)
+        #self.content_panel.add_component(SignupDialog_V2(h, num_stage, pour_stage), full_width_row=True)
+        self.content_panel.add_component(SignupDialog_V2(h, num_stage, pour_stage)
 
     def bt_se_deconnecter_click(self, **event_args):
         """This method is called when the button is clicked"""
