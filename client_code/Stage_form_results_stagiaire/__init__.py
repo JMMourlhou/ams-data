58b077d365b756eeb392ff7aa409b4fa86cd3bdc
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-11-05 16:19:30 +0000

    Edited Stage_form_results_stagiaire

--- client_code/Stage_form_results_stagiaire/__init__.py
+++ client_code/Stage_form_results_stagiaire/__init__.py
@@ -84,7 +84,6 @@ class Stage_form_results_stagiaire(Stage_form_results_stagiaireTemplate):
         liste_formulaires = app_tables.com.search(date=self.date["date"])
         nb_formulaires = len(liste_formulaires)
         max_points_ferm = nb_formulaires * 6      # max de points possibles pour une question
-        alert(f"max points fermés: {max_points_ferm}")
         #  ================================================================  Affichage résultats
         # extraction du stage par lecture du premier formulaire de la liste
         first_row = liste_formulaires[0]
@@ -97,8 +96,7 @@ class Stage_form_results_stagiaire(Stage_form_results_stagiaireTemplate):
         #alert(f"nb de questions ferm : {nb_questions_ferm}")
         # nb_questions_ouv
         nb_questions_ouv = int(dico_q_ouv["NBQ"])  # nb de questions fermées ds le dico
-        #alert(f"nb de question ouv: {nb_questions_ouv}")
-
+        alert(f"nb de question ouv: {nb_questions_ouv}")
         
         # =========================== Boucle sur tous les formulaires pour les questions fermées
         # initialisation des compteurs
@@ -112,77 +110,112 @@ class Stage_form_results_stagiaire(Stage_form_results_stagiaireTemplate):
         points_q8 = 0
         points_q9 = 0
         points_q10 = 0
+
+        rep_ouv1 = ""
+        rep_ouv2 = ""
+        rep_ouv3 = ""
+        rep_ouv4 = ""
+        rep_ouv5 = ""
+        rep_ouv6 = ""
+        rep_ouv7 = ""
+        rep_ouv8 = ""
+        rep_ouv9 = ""
+        rep_ouv10 = ""
+
         
         # boucle
         for formulaire in liste_formulaires:
-            dico_ferm = formulaire["com_ferm"]   # dico fermé du formulaire
             
+            dico_ferm = formulaire["com_ferm"]   # dico fermé du formulaire
+            # cumul des scores pour questions fermées
             points_q1 = points_q1 + dico_ferm["1"][1] + 1 # cumul 1ere question  + 1 car mini = 0, max = 5
-            #alert(f"cumul q1: {points_q1}")
             points_q2 = points_q2 + dico_ferm["2"][1] + 1  # cumul 2eme question
-            #alert(f"cumul q2: {points_q2}")
             points_q3 = points_q3 + dico_ferm["3"][1] + 1 # cumul 3eme question
-            #alert(f"cumul q3: {points_q3}")
             points_q4 = points_q4 + dico_ferm["4"][1] + 1 # cumul 4eme question
-            #alert(f"cumul q4: {points_q4}")
             points_q5 = points_q5 + dico_ferm["5"][1] + 1 # cumul 5eme question
-            #alert(f"cumul q5: {points_q5}")
             points_q6 = points_q6 + dico_ferm["6"][1] + 1 # cumul 6eme question
-            #alert(f"cumul q6: {points_q6}")
             points_q7 = points_q7 + dico_ferm["7"][1] + 1 # cumul 7eme question
-            #alert(f"cumul q7: {points_q7}")
             points_q8 = points_q8 + dico_ferm["8"][1] + 1 # cumul 8eme question
-            #alert(f"cumul q8: {points_q8}")
             points_q9 = points_q9 + dico_ferm["9"][1] + 1 # cumul 9eme question
-            #alert(f"cumul q9: {points_q9}")
             points_q10 = points_q10 + dico_ferm["10"][1] + 1 # cumul 10eme question
-            #alert(f"cumul q10: {points_q10}")
+            
+        # cumul des réponses pour questions ouvertes
+            dico_ouv = formulaire["com_ouv"]   # dico fermé du formulaire
+            rep_ouv1 = rep_ouv1 + "\n" + dico_ouv["1"][1]
+            rep_ouv2 = rep_ouv2 + "\n" + dico_ouv["2"][1]
+            rep_ouv3 = rep_ouv3 + "\n" + dico_ouv["3"][1]
+            rep_ouv4 = rep_ouv4 + "\n" + dico_ouv["4"][1]
+            rep_ouv5 = rep_ouv5 + "\n" + dico_ouv["5"][1]
+            rep_ouv6 = rep_ouv6 + "\n" + dico_ouv["6"][1]
+            rep_ouv7 = rep_ouv7 + "\n" + dico_ouv["7"][1]
+            rep_ouv8 = rep_ouv8 + "\n" + dico_ouv["8"][1]
+            rep_ouv9 = rep_ouv9 + "\n" + dico_ouv["9"][1]
+            rep_ouv10 = rep_ouv10 + "\n" + dico_ouv["10"][1]
             
         # fin de boucle questions fermées, calcul des pourcentages
-        pourcent_q1 = str(round((points_q1 / max_points_ferm) * 100,2))
-        pourcent_q2 = str(round((points_q2 / max_points_ferm) * 100,2))
-        pourcent_q3 = str(round((points_q3 / max_points_ferm) * 100,2))
-        pourcent_q4 = str(round((points_q4 / max_points_ferm) * 100,2))
-        pourcent_q5 = str(round((points_q5 / max_points_ferm) * 100,2))
-        pourcent_q6 = str(round((points_q6 / max_points_ferm) * 100,2))
-        pourcent_q7 = str(round((points_q7 / max_points_ferm) * 100,2))
-        pourcent_q8 = str(round((points_q8 / max_points_ferm) * 100,2))
-        pourcent_q9 = str(round((points_q9 / max_points_ferm) * 100,2))
-        pourcent_q10 = str(round((points_q10 / max_points_ferm) * 100,2))
-        # Détermination des couleurs d'affichage en fonction du pourcent
-        self.couleurs(1,pourcent_q1)
+        pourcent_q1 = round((points_q1 / max_points_ferm) * 100,2)
+        pourcent_q2 = round((points_q2 / max_points_ferm) * 100,2)
+        pourcent_q3 = round((points_q3 / max_points_ferm) * 100,2)
+        pourcent_q4 = round((points_q4 / max_points_ferm) * 100,2)
+        pourcent_q5 = round((points_q5 / max_points_ferm) * 100,2)
+        pourcent_q6 = round((points_q6 / max_points_ferm) * 100,2)
+        pourcent_q7 = round((points_q7 / max_points_ferm) * 100,2)
+        pourcent_q8 = round((points_q8 / max_points_ferm) * 100,2)
+        pourcent_q9 = round((points_q9 / max_points_ferm) * 100,2)
+        pourcent_q10 = round((points_q10 / max_points_ferm) * 100,2)
+        # pour chq question, détermination des couleurs d'affichage en fonction du pourcent
+        nom_couleur1 = self.couleurs(pourcent_q1)
+        nom_couleur2 = self.couleurs(pourcent_q2)
+        nom_couleur3 = self.couleurs(pourcent_q3)
+        nom_couleur4 = self.couleurs(pourcent_q4)
+        nom_couleur5 = self.couleurs(pourcent_q5)
+        nom_couleur6 = self.couleurs(pourcent_q6)
+        nom_couleur7 = self.couleurs(pourcent_q7)
+        nom_couleur8 = self.couleurs(pourcent_q8)
+        nom_couleur9 = self.couleurs(pourcent_q9)
+        nom_couleur10 = self.couleurs(pourcent_q10)
+        self.label_1.background = nom_couleur1
+        self.label_2.background = nom_couleur2
+        self.label_3.background = nom_couleur3
+        self.label_4.background = nom_couleur4
+        self.label_5.background = nom_couleur5
+        self.label_6.background = nom_couleur6
+        self.label_7.background = nom_couleur7
+        self.label_8.background = nom_couleur8
+        self.label_9.background = nom_couleur9
+        self.label_10.background = nom_couleur10
         
         # affichage des formes fermées  en fonction de leur nb
         if nb_questions_ferm > 0:  # Check du nb de questions fermées à afficher et affectation des questions
             self.column_panel_1.visible = True
-            self.label_1.text = dico_q_ferm["1"][0] + ": " + pourcent_q1 + " %"  # Je prends le 1er elmt de la liste (la question), le 2eme: si question 'obligatoire / facultative'
+            self.label_1.text = dico_q_ferm["1"][0] + ": " + str(pourcent_q1) + " %"  # Je prends le 1er elmt de la liste (la question), le 2eme: si question 'obligatoire / facultative'
         if nb_questions_ferm > 1:
             self.column_panel_2.visible = True
-            self.label_2.text = dico_q_ferm["2"][0] + ": " + pourcent_q2 + " %" 
+            self.label_2.text = dico_q_ferm["2"][0] + ": " + str(pourcent_q2) + " %" 
         if nb_questions_ferm > 2:
             self.column_panel_3.visible = True
-            self.label_3.text = dico_q_ferm["3"][0] + ": " + pourcent_q3 + " %" 
+            self.label_3.text = dico_q_ferm["3"][0] + ": " + str(pourcent_q3) + " %" 
         if nb_questions_ferm > 3:
             self.column_panel_4.visible = True
-            self.label_4.text = dico_q_ferm["4"][0] + ": " + pourcent_q4 + " %" 
+            self.label_4.text = dico_q_ferm["4"][0] + ": " + str(pourcent_q4) + " %" 
         if nb_questions_ferm > 4:
             self.column_panel_5.visible = True
-            self.label_5.text = dico_q_ferm["5"][0] + ": " + pourcent_q5 + " %" 
+            self.label_5.text = dico_q_ferm["5"][0] + ": " + str(pourcent_q5) + " %" 
         if nb_questions_ferm > 5:
             self.column_panel_6.visible = True
-            self.label_6.text = dico_q_ferm["6"][0] + ": " + pourcent_q6 + " %" 
+            self.label_6.text = dico_q_ferm["6"][0] + ": " + str(pourcent_q6) + " %" 
         if nb_questions_ferm > 6:
             self.column_panel_7.visible = True
-            self.label_7.text = dico_q_ferm["7"][0] + ": " + pourcent_q7 + " %" 
+            self.label_7.text = dico_q_ferm["7"][0] + ": " + str(pourcent_q7) + " %" 
         if nb_questions_ferm > 7:
             self.column_panel_8.visible = True
-            self.label_8.text = dico_q_ferm["8"][0] + ": " + pourcent_q8 + " %" 
+            self.label_8.text = dico_q_ferm["8"][0] + ": " + str(pourcent_q8) + " %" 
         if nb_questions_ferm > 8:
             self.column_panel_9.visible = True
-            self.label_9.text = dico_q_ferm["9"][0] + ": " + pourcent_q9 + " %" 
+            self.label_9.text = dico_q_ferm["9"][0] + ": " + str(pourcent_q9) + " %" 
         if nb_questions_ferm > 9:
             self.column_panel_10.visible = True
-            self.label_10.text = dico_q_ferm["10"][0] + ": " + pourcent_q10 + " %" 
+            self.label_10.text = dico_q_ferm["10"][0] + ": " + str(pourcent_q10) + " %" 
 
         # affichage des formes ouvertes en fonction de leur nb
         dico_q_ouv = stage_row["com_ouv"]  # check du nb de questions ouvertes à afficher et affectation des questions
@@ -228,28 +261,18 @@ class Stage_form_results_stagiaire(Stage_form_results_stagiaireTemplate):
             self.column_panel_a10.visible = True
             self.label_a10.text = dico_q_ouv["10"][0]
             
-    def couleurs(self, num_question, pourcent,  **event_args):    
+    def couleurs(self, pourcent,  **event_args):    
         if pourcent<=16: 
-        nom_couleur = "theme:Error"
-        if num_question == 1:
-            self.label_1.background = nom_couleur
-        if num_question == 2:
-            self.label_2.background = nom_couleur
-        if num_question == 3:
-            self.label_3.background = nom_couleur
-        if num_question == 4:
-            self.label_4.background = nom_couleur
-        if num_question == 5:
-            self.label_5.background = nom_couleur
-        if num_question == 6:
-            self.label_6.background = nom_couleur
-        if num_question == 7:
-            self.label_7.background = nom_couleur
-        if num_question == 8:
-            self.label_8.background = nom_couleur
-        if num_question == 9:
-            self.label_9.background = nom_couleur
-        if num_question == 10:
-            self.label_10.background = nom_couleur
-        return 
-    
+            nom_couleur = "theme:Error"
+        if pourcent<=32 and pourcent > 16: 
+            nom_couleur = "theme:Orange"
+        if pourcent<=48 and pourcent > 32: 
+            nom_couleur = "theme:Jaune Orange"
+        if pourcent<=64 and pourcent > 48: 
+            nom_couleur = "theme:Vert Tres Clair"
+        if pourcent<=80 and pourcent > 64: 
+            nom_couleur = "theme:Vert Clair"
+        if pourcent > 80: 
+            nom_couleur = "theme:Vert Clair"
+        return nom_couleur
+        
