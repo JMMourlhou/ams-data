8e49ed6aa363c0b5fe4f6dc0af2f39d5de382afe
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2025-05-19 16:10:07 +0000

    Edited Visu_stages.RowTemplate3

--- client_code/Visu_stages/RowTemplate3/__init__.py
+++ client_code/Visu_stages/RowTemplate3/__init__.py
@@ -139,7 +139,14 @@ class RowTemplate3(RowTemplate3Template):
         liste_stagiaires = app_tables.stagiaires_inscrits.search(
                                                                 tables.order_by("name", ascending=True),
                                                                 numero=self.item['numero'])
-        # envoi vers pi5 par uplink du num_stage et de la liste des stgiaires
+        # envoi vers pi5 par uplink du mail saisi 
+        mail = "jmmourlhou@gmail.com"
+        result = anvil.server.call('get_mail_dest', mail)
+        if result is False:
+            alert("Le mail n'a pas le bon format !")
+            return
+            
+        # envoi vers pi5 par uplink du num_stage et de la liste des stagiaires
         message = anvil.server.call('export_xls', self.item['numero'], self.item['code_txt'], self.item['date_debut'] , liste_stagiaires, "jmmourlhou@gmail.com")
         alert(message)
   
